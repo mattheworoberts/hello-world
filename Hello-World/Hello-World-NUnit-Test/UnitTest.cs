84566875711dbca33d4619beedac9961bacfd8ac
John H. Freeman <john@freemania.pro>
2024-01-21 00:00:52 -0500

    NUnit UWP test added

---
+++ Hello-World/Hello-World-NUnit-Test/UnitTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hello_World;
+
+namespace Hello_World_NUnit_Test
+{
+    [TestClass]
+    public class HelloWorldViewModelTests
+    {
+        HelloWorldViewModel hw;
+
+        [TestInitialize]
+        public void initializeTest()
+        {
+            hw = new HelloWorldViewModel();
+        }
+
+        [TestMethod]
+        public void TestHelloWorldMessageProperty()
+        {
+            const string msg = "Test String";
+            hw.helloWorldViewModel = msg;
+            Assert.AreEqual(hw.helloWorldViewModel, msg);
+            Assert.AreNotEqual(hw.helloWorldViewModel, null);
+        }
+    }
+}
